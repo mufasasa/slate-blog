8e16f58d7b0d4b4b4c6ab254e901c0e1aa4d59ab
Mohammed <mohammed.dahiru72@gmail.com>
2023-03-07 19:22:53 +0100

    fix db conn string

--- slate_blog/settings/production.py
+++ slate_blog/settings/production.py
@@ -9,14 +9,7 @@ except ImportError:
     pass
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'slate_blog',
-        'USER': 'slate_blog',
-        'PASSWORD': 'slate_blog',
-        'HOST': 'slate-pgres-do-user-464771-0.b.db.ondigitalocean.com',
-        'PORT': '25060',
-    }
+    'default': dj_database_url.parse("postgresql://doadmin:zajUk66uWoSCBrCr@slate-pgres-do-user-464771-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
 }
 
 ALLOWED_HOSTS = ["*"]
