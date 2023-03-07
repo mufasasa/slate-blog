78fd7fd14744918a05a5ac44e0f37fb4f49dbcb9
Mohammed <mohammed.dahiru72@gmail.com>
2023-03-07 18:20:41 +0100

    setting up

--- slate_blog/settings/production.py
+++ slate_blog/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -6,3 +7,16 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'slate_blog',
+        'USER': 'slate_blog',
+        'PASSWORD': 'slate_blog',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+ALLOWED_HOSTS = ["*"]
